9036053bc06eef6f165fdd2dcdf00f02092fd1ae
matesu <mcos.mateus@gmail.com>
2025-10-03 00:14:34 -0300

    Alteração da interface, criação dos eventos de abrir janelas, efeitos de hover, algoritmo de conversão string/icon, estruturação de arquivos

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using patrimonio_digital.Core;
+using patrimonio_digital.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace patrimonio_digital.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public ICommand AbrirJanelaCommand { get; }
+
+        public MainViewModel()
+        {
+            AbrirJanelaCommand = new RelayCommand(AbrirJanela);
+        }
+
+        private void AbrirJanela(object parameter)
+        {
+            if (parameter is string tipoJanela)
+            {
+                Window janela = tipoJanela switch
+                {
+                    "Catalogar" => new CatalogarItemWindow(),
+                    "Auditoria" => new Auditoria(),
+                    "Usuarios" => new Usuarios(),
+                    _ => null
+                };
+
+                janela?.Show();
+            }
+        }
+    }
+
+}
---
+++ Utils/StringToIcon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using FontAwesome.WPF;
+
+// conversão de string pro formato lido pelo fontawesome 
+
+namespace patrimonio_digital.Utils
+{
+    public class StringToIcon : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && Enum.TryParse(typeof(FontAwesomeIcon), str, out var icon))
+                return icon;
+            return FontAwesomeIcon.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+    }
+}
